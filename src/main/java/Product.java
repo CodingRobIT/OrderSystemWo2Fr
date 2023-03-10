93e745462203394c671eb89cf4eccf611c6f687f
Robin Schatzl <tmai02476@gmail.com>
2023-03-10 10:34:18 +0100

    added empty Main + Product as Class complete

---
+++ src/main/java/Product.java
@@ -0,0 +1,55 @@
+public class Product {
+
+    int id;
+    String name;
+
+    private Product() {
+    }
+
+    private Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+
+        if (getId() != product.getId()) return false;
+        return getName() != null ? getName().equals(product.getName()) : product.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+
