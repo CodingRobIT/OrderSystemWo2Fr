42ce2566e0c342f8486f82a7d30eaa78f8259037
Robin Schatzl <tmai02476@gmail.com>
2023-03-10 12:11:25 +0100

    added Test

---
+++ src/test/java/ProductRepoTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepoTest {
+
+    @Test
+    void returnAllProdukts() {
+        //Given
+        Product product1 = new Product("B10", "Becher");        //Anlegen zweier Produkte
+        Product product2 = new Product("G15", "Glass");         //Hier Produkt 2
+        Map<String, Product>productTestMap = new HashMap<>();   //
+        productTestMap.put(product1.getId(),product1);
+        productTestMap.put(product2.getId(),product2);
+        ProductRepo productRepo1 = new ProductRepo(productTestMap);
+
+        //WHEN
+        Map<String, Product> actual = productRepo1.list();
+        //THEN
+        assertEquals(productTestMap, actual);
+    }
+
+    @Test
+    void returnNoProdukts() {
+        //Given
+        //Product product1 = new Product("B10", "Becher");        //Anlegen zweier Produkte
+        //Product product2 = new Product("G15", "Glass");         //Hier Produkt 2
+        Map<String, Product>productTestMap = new HashMap<>();
+        //productTestMap.put(product1.getId(),product1);
+        //productTestMap.put(product2.getId(),product2);
+        ProductRepo productRepo1 = new ProductRepo(productTestMap);
+
+
+        //WHEN
+        Map<String, Product> actual = productRepo1.list();
+
+        //THEN
+        assertEquals(Collections.emptyMap(), actual);
+    }
+
+
+}
