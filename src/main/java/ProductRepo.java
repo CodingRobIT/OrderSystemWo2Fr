af38a69cd69fc750dde2bd860a238202891f9a61
Robin Schatzl <tmai02476@gmail.com>
2023-03-10 11:03:42 +0100

    added ProductRepo

---
+++ src/main/java/ProductRepo.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.Map;
+
+public class ProductRepo {
+    private Map<String, Product> productMap;
+
+    public ProductRepo(Map<String, Product> productMap) {
+        this.productMap = productMap;
+    }
+
+    public Map<String, Product> list() {
+        return productMap;
+    }
+
+    public Product get(String id) {
+        return productMap.get(id);
+    }
+
+
+}
